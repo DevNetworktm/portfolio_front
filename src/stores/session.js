c1acdb824025699ade9d8cfc1bf7886308d92205
ZacharyMasson <m.zachary@spinforeat.com>
2023-01-05 00:59:20 +0100

    [STORES][ADD]
    + useSessionStore

---
+++ src/stores/session.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+const useSessionStore = defineStore({
+    id: "session",
+    state: () => ({
+
+    }),
+    getters: {
+
+    },
+    actions: {
+
+    }
+});
+
+export default useSessionStore;
