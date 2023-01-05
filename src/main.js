505f8d2185322d22419066a3b0d3ccc32e73b3c4
ZacharyMasson <m.zachary@spinforeat.com>
2023-01-05 01:00:18 +0100

    [MAIN_VUE][EDIT]
    + vue-router
    + pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import Router from '@/routes/index';
+
+createApp(App)
+    .use(Router)
+    .use(createPinia())
+    .mount('#app');
